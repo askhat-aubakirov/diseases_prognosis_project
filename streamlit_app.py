780ed89acaaf5ce3c131a548257acbbf747e4bf6
Askhat Aubakirov <105732426+askhat-aubakirov@users.noreply.github.com>
2024-03-22 22:37:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib #для загрузки готовой модели
 
 
-st.write("Загрузка натренированной RandomForest (Случайный Лес)...")
+st.write("Это приложение использует натренированную модель с использованием RandomForestClassifier и датасета Disease Prediction Using Machine Learning \nhttps://www.kaggle.com/datasets/kaushil268/disease-prediction-using-machine-learning/data \nЗагрузка натренированной RandomForest (Случайный Лес)...")
 with st.spinner("Загрузка..."):
     loaded_model = joblib.load('model_random_f.joblib')
 
