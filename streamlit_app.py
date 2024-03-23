9966ccf2f7813da4a4340162d4de65de000f91f1
Askhat Aubakirov <105732426+askhat-aubakirov@users.noreply.github.com>
2024-03-23 16:19:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ if st.button("Прогноз"):
     # Assuming model expects numerical features, convert selected symptoms to a list of 1s and 0s
     # based on presence/absence in the selected_symptoms list
     features = [1 if symptom in selected_symptoms else 0 for symptom in symptoms.keys()]
-    st.write(f"Входные данные для модели в виде списка (без названий столбцов):\n\n{features}, \n\nДлина входного набора данных:\n\n{len(features)} \n\nВыбранные симптомы (из оригинальные названия из датасета): {selected_symptoms}\n\n")
+    st.write(f"Входные данные для модели в виде списка (без названий столбцов):\n\n{features}, \n\nДлина входного набора данных:\n\n{len(features)} \n\nВыбранные симптомы (их оригинальные названия из датасета): {selected_symptoms}\n\n")
     prediction = loaded_model.predict([features])
     st.header(f"Прогноз: {prediction[0]}", divider='rainbow')
     st.write("Помните, что эта программа сделана в целях демонстрации использования технологий в медицине и не является заменой профессиональной медицинской консультации")
