60477e3b66014db556bb4c5eeac304da2962bed0
Askhat Aubakirov <105732426+askhat-aubakirov@users.noreply.github.com>
2024-03-23 12:34:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,7 +159,6 @@ if st.button("Прогноз"):
     # Assuming model expects numerical features, convert selected symptoms to a list of 1s and 0s
     # based on presence/absence in the selected_symptoms list
     features = [1 if symptom in selected_symptoms else 0 for symptom in symptoms.keys()]
-    features.append(0) #ГРУБАЯ ПРОВЕРКА ДЛЯ ПРЕДИКТА - TODO: стоит убрать позднее
     st.write(f"{features}, length: {len(features)} \n{selected_symptoms}\n\n")
     prediction = loaded_model.predict([features])
     st.write("Прогноз:", prediction[0])
